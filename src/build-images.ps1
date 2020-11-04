380351d2a6432ae483c6423651b7624bd8afe251
Moim HA <moimhossain@gmail.com>
2020-11-04 07:52:27 +0100

    Added buld and deploy sh

--- src/build-images.ps1
+++ src/build-images.ps1
@@ -1,6 +1,61 @@
+#!/bin/bash
+DockerImageTag=$1
+DockerPassword=$2
 
-docker build -t moimhossain/dapr-backend:v1 -f .\Dapr.Backend\Dockerfile .
-docker push moimhossain/dapr-backend:v1
+echo " "
+echo "Preparing manifests with build Tag $DockerImageTag"
 
-docker build -t moimhossain/dapr-frontend:v1 -f .\Dapr.Frontend\Dockerfile .
-docker push moimhossain/dapr-frontend:v1
+echo " "
+echo "Build Tag $DockerImageTag received"
+echo "┌┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┐"
+echo "┊ Building images (Backend)                          │"
+echo "└┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘"
+docker build -t moimhossain/dapr-backend:$DockerImageTag -f .\Dapr.Backend\Dockerfile . 
+
+echo "┌┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┐"
+echo "┊ Building images (Frontend)                         │"
+echo "└┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘"
+docker build -t moimhossain/dapr-frontend:$DockerImageTag -f .\Dapr.Frontend\Dockerfile .
+
+echo " "
+echo "┌┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┐"
+echo "┊ Pushing images (Backend)                           │"
+echo "└┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘"
+docker push moimhossain/dapr-backend:$DockerImageTag
+
+echo " "
+echo "┌┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┐"
+echo "┊ Pushing images (Frontend)                          │"
+echo "└┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘"
+docker push moimhossain/dapr-frontend:$DockerImageTag
+
+
+echo "Changing directory.."
+cd manifests
+
+echo "Replaceing Image tags.."
+sed -i "s/latest/$DockerImageTag/" backend.yaml
+sed -i "s/latest/$DockerImageTag/" frontend.yaml
+
+echo "------------------Backend end-------------------------------"
+cat backend.yaml
+
+echo "------------------Front end-------------------------------"
+cat frontend.yaml
+
+echo " "
+echo "┌┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┐"
+echo "┊ Applying to Kubernetes                             │"
+echo "└┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘"
+
+kubectl apply -f namespace.yaml
+
+kubectl -n dapr-workload create secret docker-registry dockerhub --docker-server=https://index.docker.io/v1/ --docker-username=moimhossain --docker-password=$DockerPassword --docker-email=moimhossain@gmail.com
+
+
+kubectl apply -f redis.yaml
+kubectl apply -f zipkin.yaml
+
+
+kubectl apply -f backend.yaml
+kubectl apply -f frontend.yaml
